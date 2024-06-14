7a99bbbc38072bd159252e9a5b66ec9241ac7016
Manuel Escalante <maescalantehe@gmail.com>
2024-06-14 11:08:42 -0400

    refactor: fromArabic method

--- src/RomanNumerals.ts
+++ src/RomanNumerals.ts
@@ -2,22 +2,15 @@ export class RomanNumerals {
   fromArabic(value: number): string {
     let result: string = ""
 
-    if (value <= 3) {
-      for (let i = 1; i <= value; i++) {
-        result += "I"
-      }
-    }
+    if (value === 4) return "IV"
 
-    if (value === 4) {
-      result = "IV"
-    }
-
-    if (value === 5) {
+    if (value >= 5) {
       result = "V"
+      value -= 5
     }
 
-    if (value === 6) {
-      result = "VI"
+    for (let i = 1; i <= value; i++) {
+      result += "I"
     }
 
     return result
